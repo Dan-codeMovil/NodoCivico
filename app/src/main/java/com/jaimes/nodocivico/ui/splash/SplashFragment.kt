3b6959e40ff0337eee687985b39d358ddcda8a4e
Daniel <danej1331@gmail.com>
2026-05-14 07:13:41 -0500

    Estructura y navigation

---
+++ app/src/main/java/com/jaimes/nodocivico/ui/splash/SplashFragment.kt
@@ -0,0 +1,19 @@
+package com.jaimes.nodocivico.ui.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jaimes.nodocivico.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+}
