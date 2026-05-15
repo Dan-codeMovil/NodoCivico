9aacd33fa228b321c9052bcc218b11d3eb6d19a4
Daniel <danej1331@gmail.com>
2026-05-14 22:51:02 -0500

    Creacion boton en login y modificar LoginFragment.kt

--- app/src/main/java/com/jaimes/nodocivico/ui/login/LoginFragment.kt
+++ app/src/main/java/com/jaimes/nodocivico/ui/login/LoginFragment.kt
@@ -1,10 +1,10 @@
 package com.jaimes.nodocivico.ui.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.jaimes.nodocivico.R
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
@@ -12,5 +12,12 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val btnLogin = view.findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_loginFragment_to_homeFragment
+            )
+        }
     }
 }
