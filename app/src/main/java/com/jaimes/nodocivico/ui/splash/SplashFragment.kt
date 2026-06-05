10127fec0471647eb491bda5cbf58412bb954e17
danny <danieleduardojaimes@uts.edu.co>
2026-06-04 20:54:16 -0500

    Arreglos

--- app/src/main/java/com/jaimes/nodocivico/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/jaimes/nodocivico/ui/splash/SplashFragment.kt
@@ -1,10 +1,9 @@
 package com.jaimes.nodocivico.ui.splash
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.jaimes.nodocivico.R
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
@@ -12,5 +11,12 @@ class SplashFragment : Fragment(R.layout.fragment_splash) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        view.postDelayed({
+
+            findNavController().navigate(
+                R.id.action_splashFragment_to_loginFragment
+            )
+
+        }, 2000)
     }
 }
